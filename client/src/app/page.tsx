3ba6f1fccf0ea82f8ee00ff0fddfcf9df6279055
izumii <154342465+izumiCode@users.noreply.github.com>
2025-02-09 22:13:44 +0530

    configured theme

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { ThemeModeToggle } from "@/components/mode-toggler";
+
 const page = () => {
-  return <div>INIT</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <ThemeModeToggle />
+    </div>
+  );
 };
 
 export default page;
